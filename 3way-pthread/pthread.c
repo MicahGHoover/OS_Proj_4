2f7d3a9d61e13d1cb9367521120d9a7650cad34d
MicahGHoover <89356936+MicahGHoover@users.noreply.github.com>
2024-04-06 15:38:32 -0500

    Update pthread.c
    an attempt at the find max ascii value function

--- 3way-pthread/pthread.c
+++ 3way-pthread/pthread.c
@@ -4,6 +4,36 @@
 #include <pthread.h>
 #include <string.h>
 
+// Structure to hold information for each thread
+struct ThreadData {
+    char *line;
+    int max_ascii;
+};
+
+// Potential Function to find maximum ASCII value in a line
+// Pass this function into the pthread create in the main
+void *find_max_ascii(void *arg) {
+    // Cast the argument to the appropriate type
+    struct ThreadData *data = (struct ThreadData *)arg;
+
+    // Initialize max_ascii to 0
+    data->max_ascii = 0;
+    
+    // Loop through each character in the line until the null terminator ('\0') is encountered
+    for (int i = 0; data->line[i] != '\0'; i++) {
+        // Retrieve the ASCII value of the current character
+        int ascii_value = (int)data->line[i];
+        
+        // If the ASCII value of the current character is greater than the current max_ascii value, update max_ascii
+        if (ascii_value > data->max_ascii) {
+            data->max_ascii = ascii_value;
+        }
+    }
+    
+    // Exit the thread
+    pthread_exit(NULL);
+}
+
 int main(int argc, char *argv[])
 {
   if (argc != 2) {
